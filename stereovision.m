0c7d88cab2db68e3dd977ed2de0261a929a10418
quyuyi <quyuyi@umich.edu>
2020-06-18 19:53:32 -0400

    stereo calibration 0618

--- stereovision.m
+++ stereovision.m
@@ -4,96 +4,78 @@ image_counter = 1;
 addpath(pwd);
 addpath(strcat(pwd,'/utils'));
 
-RGB = imread(strcat(pwd, '/data/dataSmall/RGBImage_8.png'));
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(RGB)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('RGB')
+RGB = imread(strcat(pwd, '/data/data0618_1/RGBImage_7.png'));
+grayfromRGB = rgb2gray(RGB); % turn RGB to gray
+IR = imread(strcat(pwd, '/data/data0618_1/GrayImage_7.png'));
 
-% turn RGB to gray
-grayfromRGB = rgb2gray(RGB);
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(grayfromRGB)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('gray image generated from RGB')
-
-IR = imread(strcat(pwd, '/data/dataSmall/GrayImage_8.png'));
+% scale intensity of ir and gray image
+IR = histeq(IR);
+grayfromRGB = histeq(grayfromRGB);
 % figure(image_counter);
 % image_counter = image_counter + 1;
 % imagesc(IR)
 % set(gca,'dataAspectRatio',[1 1 1])
-% title('IR')
-
-% scale intensity of ir and gray image
-IR = histeq(IR);
-figure(image_counter);
-image_counter = image_counter + 1;
-imagesc(IR)
-set(gca,'dataAspectRatio',[1 1 1])
-title('Enhanced the contrast of IR')
-
-grayfromRGB = histeq(grayfromRGB);
-figure(image_counter);
-image_counter = image_counter + 1;
-imagesc(grayfromRGB)
-set(gca,'dataAspectRatio',[1 1 1])
-title('Enhanced the contrast of grayfromRGB')
+% title('Enhanced the contrast of IR')
+% figure(image_counter);
+% image_counter = image_counter + 1;
+% imagesc(grayfromRGB)
+% set(gca,'dataAspectRatio',[1 1 1])
+% title('Enhanced the contrast of grayfromRGB')
 
 %% Rectify based on uncalibrated stereo cameras
-blobs1 = detectSURFFeatures(grayfromRGB, 'MetricThreshold', 2000);
-blobs2 = detectSURFFeatures(IR, 'MetricThreshold', 2000);
-figure;
-imshow(grayfromRGB);
-hold on;
-plot(selectStrongest(blobs1, 30));
-title('Thirty strongest SURF features in I1');
 
-figure;
-imshow(IR);
-hold on;
-plot(selectStrongest(blobs2, 30));
-title('Thirty strongest SURF features in I2');
+% blobs1 = detectSURFFeatures(grayfromRGB, 'MetricThreshold', 2000);
+% blobs2 = detectSURFFeatures(IR, 'MetricThreshold', 2000);
+% figure;
+% imshow(grayfromRGB);
+% hold on;
+% plot(selectStrongest(blobs1, 30));
+% title('Thirty strongest SURF features in I1');
+% 
+% figure;
+% imshow(IR);
+% hold on;
+% plot(selectStrongest(blobs2, 30));
+% title('Thirty strongest SURF features in I2');
 
-% %% Rectify based on calibration
-% load('calibration/stereoCalibrationParams.mat');
-% [RGBRect, IRRect] = rectifyStereoImages(grayfromRGB, IR, stereoParams, 'OutputView','full');
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(RGBRect)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('grayfromRGB Rectified')
-% 
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(IRRect)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('IR Rectified')
-% 
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(stereoAnaglyph(RGBRect, IRRect));
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('Rectified Frames');
-% 
-% %% Compute Disparity
-% disparityMap = disparitySGM(RGBRect, IRRect);
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(disparityMap, [0, 64]);
-% title('Disparity Map');
-% colormap jet
-% colorbar
-% 
-% %% Reconstruct the 3-D Scene
-% points3D = reconstructScene(disparityMap, stereoParams);
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% pcshow(points3D)
-% title('Pointcloud')
-% xlabel('X')
-% ylabel('Y')
-% zlabel('Z')
+%% Rectify based on calibration
+load('calibration/stereoCalibrationParams0618_2.mat');
+[RGBRect, IRRect] = rectifyStereoImages(grayfromRGB, IR, stereoParams, 'OutputView','full');
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(RGBRect)
+set(gca,'dataAspectRatio',[1 1 1])
+title('grayfromRGB Rectified')
+
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(IRRect)
+set(gca,'dataAspectRatio',[1 1 1])
+title('IR Rectified')
+
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(stereoAnaglyph(RGBRect, IRRect));
+set(gca,'dataAspectRatio',[1 1 1])
+title('Rectified Frames');
+
+%% Compute Disparity
+disparityMap = disparitySGM(RGBRect, IRRect);
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(disparityMap, [0, 64]);
+title('Disparity Map');
+colormap jet
+colorbar
+
+%% Reconstruct the 3-D Scene
+points3D = reconstructScene(disparityMap, stereoParams);
+figure(image_counter);
+image_counter = image_counter + 1;
+pcshow(points3D)
+title('Pointcloud')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
 
 
