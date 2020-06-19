23cba59b65df753a959d726a82a9a0619ec08a17
quyuyi <quyuyi@umich.edu>
2020-06-19 02:19:10 -0400

    wo commit le

--- image_alignment.m
+++ image_alignment.m
@@ -5,8 +5,8 @@ addpath(pwd);
 addpath(strcat(pwd,'/utils'));
 addpath(strcat(pwd,'/calibration'));
 
-D1 = imread(strcat(pwd, '/data/0605/DepthImage_3.png'));
-D2 = imread(strcat(pwd, '/data/0605/DepthImage_1.png'));
+D1 = imread(strcat(pwd, '/data/data0618_1/DepthImage_5.png'));
+D2 = imread(strcat(pwd, '/data/data0618_1/DepthImage_8.png'));
 D1 = D1/16;
 D2 = D2/16;
 load('calibration/ir.mat');
@@ -16,41 +16,19 @@ D2 = undistortImage(D2,ircameraParams);
 
 figure(image_counter);
 image_counter = image_counter + 1;
-imagesc(D1)
-set(gca,'dataAspectRatio',[1 1 1])
-title('origiunal depth images');
-
-figure(image_counter);
-image_counter = image_counter + 1;
 imshowpair(D1, D2,'Scaling','joint')
 title('misaligned depth images');
 
 %% Align two depth images
 % find transform martix from gray image
-IR1 = imread(strcat(pwd, '/data/0605/GrayImage_3.png'));
-IR2 = imread(strcat(pwd, '/data/0605/GrayImage_1.png'));
+IR1 = imread(strcat(pwd, '/data/data0618_1/GrayImage_5.png'));
+IR2 = imread(strcat(pwd, '/data/data0618_1/GrayImage_8.png'));
 
 [optimizer, metric] = imregconfig('monomodal');
 optimizer.MaximumIterations = 300;
 % [IR2_registered] = imregister(IR2, IR1, 'affine', optimizer, metric);
 tform = imregtform(IR2,IR1,'affine',optimizer,metric);
 Rfixed = imref2d(size(IR1));
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imshowpair(IR1, IR2_registered,'Scaling','joint')
-% title('aligned depth images');
-
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(IR1)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('fixed IR image');
-% 
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(IR2_registered)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('registered IR image');
 
 % apply the transform matrix on depth image
 D2_registered = imwarp(D2,tform,'OutputView',Rfixed);
@@ -60,92 +38,38 @@ image_counter = image_counter + 1;
 imshowpair(D1, D2_registered,'Scaling','joint')
 title('aligned depth images');
 
+pc1 = tof2pc(D1, C);
+pc2 = tof2pc(D2_registered, C);
 figure(image_counter);
+hold on;
 image_counter = image_counter + 1;
-imagesc(D2_registered)
-set(gca,'dataAspectRatio',[1 1 1])
-title('registered depth images');
-
-%intersection=intersect(D1,D2_registered);
-% s = size(D1);
-% w = s(1);
-% h = s(2);
-% intersection=zeros(w,h);
-% for i = 1: w
-%     for j = 1: h
-%         intersection(i,j)=0.5.*(D1(i,j)+D2_registered(i,j));
-%     end
-% end
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% imagesc(intersection)
-% set(gca,'dataAspectRatio',[1 1 1])
-% title('registered depth images');
-
-pc_original = tof2pc(D1, C);
-figure(image_counter);
-image_counter = image_counter + 1;
-pcshow(pc_original)
+pcshow(pc1, [0 1 0])
+pcshow(pc2, [0 0 1])
 title('original Pointcloud')
 xlabel('X')
 ylabel('Y')
 zlabel('Z')
 
-pc = tof2pc(intersection, C);
-figure(image_counter);
-image_counter = image_counter + 1;
-pcshow(pc)
-title('Pointcloud')
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
-
-% pc1 = tof2pc(D1, C);
-% pc2 = tof2pc(D2_registered, C);
-% pc = [pc1;pc2]; % union of two depth images
-% 
-% figure(image_counter);
-% image_counter = image_counter + 1;
-% pcshow(pc);
-% title('union Pointcloud')
-% xlabel('X')
-% ylabel('Y')
-% zlabel('Z')
-
 %% Register two point clouds using ICP algorithm
-%Construct Point Cloud
+% https://www.mathworks.com/help/vision/examples/3-d-point-cloud-registration-and-stitching.html
+% construct point cloud
 pc1 = tof2pc(D1, C);
 pc2 = tof2pc(D2, C);
 pc1 = pointCloud(pc1);
 pc2 = pointCloud(pc2);
 
-figure(image_counter);
-image_counter = image_counter + 1;
-pcshow(pc1);
-title('fixed Pointcloud')
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
+% downsample
+gridSize = 0.1;
+fixed = pcdownsample(pc1,'gridAverage', gridSize);
+moving = pcdownsample(pc2,'gridAverage', gridSize);
 
+% align using ICP
+tform = pcregistericp(moving,fixed,'Metric','pointToPlane','Extrapolate',true); % moving, fixed
+pc2_reg = pctransform(pc2,tform);
 
-figure(image_counter);
-image_counter = image_counter + 1;
-pcshowpair(pc1, pc2);
-title('Pointcloud')
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
-
-[tform,pc2_reg] = pcregistericp(pc2,pc1); % moving, fixed
-figure(image_counter);
-image_counter = image_counter + 1;
-pcshowpair(pc1, pc2_reg);
-title('Aligned Pointcloud')
-xlabel('X')
-ylabel('Y')
-zlabel('Z')
-
-pc = intersect(pc1,pc2_reg,'rows');
+% merge to construct a world scene
+mergeSize = 0.015;
+pcScene = pcmerge(pc1, pc2_reg, mergeSize);
 
 %% Align RGB and Gray imgae
 fixed = rgb2gray(imread(strcat(pwd, '/data/data0614/RGBImage_1.png')));
--- utils/stereo_calibration.m
+++ utils/stereo_calibration.m
@@ -1,5 +1,22 @@
+%% stereo calibration using toolbox
 addpath(pwd);
 addpath(strcat(pwd,'/data'));
-folder1 = strcat(pwd, '/data/calibration0618/rgb');
+addpath(strcat(pwd,'/calibration'));
+folder1 = strcat(pwd, '/data/calibration0618/rgbb');
 folder2 = strcat(pwd, '/data/calibration0618/tof');
-stereoCameraCalibrator(folder1, folder2, 13.1923);
+squareSize = 13.1923;
+stereoCameraCalibrator(folder1, folder2, squareSize);
+
+cameraCalibrator(folder1,squareSize);
+
+%% stereo calibration using parameters of each camera
+addpath(pwd);
+addpath(strcat(pwd,'/data'));
+addpath(strcat(pwd,'/calibration'));
+rgb=load('calibration/rgb0618.mat');
+tof=load('calibration/tof0618.mat');
+rotationOfCamera2 = [ 9.9996888238405357e-01, 4.9658030303482616e-03, 6.1298502306904173e-03;
+    -4.8677880138721791e-03, 9.9986169860079599e-01, -1.5902462418852185e-02;
+    -6.2079709598961298e-03, 1.5872128760654251e-02, 9.9985475776492971e-01 ];
+translationOfCamera2 = [ -3.7419189660694208e+01, -8.3045818748568878e-01, 5.9651301067425795e+00 ];
+stereoParams = stereoParameters(rgb.cameraParams, tof.cameraParams, rotationOfCamera2, translationOfCamera2);
