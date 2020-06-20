b623d337de319de941e374480d3a6936087d7ec6
AllanSJTU <50550430+AllanSJTU@users.noreply.github.com>
2020-06-20 22:33:28 +0800

    added denoising part

--- estimator_ransac.m
+++ estimator_ransac.m
@@ -7,13 +7,29 @@ image_counter = 1;
 addpath(pwd);
 addpath(strcat(pwd,'/utils'));
 addpath(strcat(pwd,'/calibration'));
-D = imread(strcat(pwd, '/data/0605/DepthImage_0.png'));
-D = D/16;
-load('calibration/fixorigincalibration.mat');
-C = cameraParams.IntrinsicMatrix';
-D_undistort = undistortImage(D,cameraParams);
 
-figure(image_counter);
+% initial dataset (that works)
+%D = imread(strcat(pwd, '/data/0605/DepthImage_3.png')); % has tried 0(good), 3(bad)
+%D = imread(strcat(pwd, '/data/data1/DepthImage_2.png')); % the picture is over-exposed
+
+% a flat dataset, taken on the table of lab
+%D = imread(strcat(pwd, '/data/dataBig/DepthImage_0.png'));
+%RGB = imread(strcat(pwd, '/data/dataBig/RGBImage_0.png'));
+
+% a good, "ground-truth" dataset
+D = imread(strcat(pwd, '/data/data0614/DepthImage_0.png'));
+IR = imread(strcat(pwd, '/data/data0614/GrayImage_0.png'));
+RGB = imread(strcat(pwd, '/data/data0614/RGBImage_0.png'));
+
+D = D/16; % depth image format is 16bit, but only 12bit is useful -> D/(2^4)
+
+load('calibration/fixorigincalibration.mat');
+C = cameraParams.IntrinsicMatrix'; % intrinsic matrix
+D_undistort = undistortImage(D,cameraParams); % a matlab built-in function,should we use it?, or, is it useless?
+
+%D_undistort = D;
+
+figure(image_counter); % a counter of numbers
 image_counter = image_counter + 1;
 imagesc(D)
 set(gca,'dataAspectRatio',[1 1 1])
@@ -24,6 +40,67 @@ image_counter = image_counter + 1;
 imagesc(D_undistort)
 set(gca,'dataAspectRatio',[1 1 1])
 title('Depth Undistorted')
+
+figure(image_counter);
+image_counter = image_counter + 1;
+IR_undistort = undistortImage(IR,cameraParams);
+imagesc(IR_undistort)
+set(gca,'dataAspectRatio',[1 1 1])
+title('IR image')
+
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(RGB)
+set(gca,'dataAspectRatio',[1 1 1])
+title('RGB image')
+
+%% Denosing - to the depth image: two paramters to specify, mu_reg and c_thres_rate
+addpath(strcat(pwd,'/denoising'));
+[imgHeight, imgWidth] = size(D_undistort);
+
+D_resize = [D_undistort; zeros(imgWidth-imgHeight, imgWidth)]; %make it square, in order to use SB_ATV
+D_unrolled = D_resize(:);
+D_unrolled = double(D_unrolled);
+
+mu_reg = 60; %   mu: regularisation parameter
+
+D_unrolled_denoise_atv = SB_ATV(D_unrolled, mu_reg); % use ATV, becuase ITV cannot run 
+
+fprintf('ATV Rel.Err = %g\n',norm(D_unrolled_denoise_atv(:) - D_unrolled(:)) ...
+    / norm(D_unrolled_denoise_atv(:)));
+
+D_denoise_atv = reshape(D_unrolled_denoise_atv, imgWidth, imgWidth);
+D_denoise_atv = D_denoise_atv(1:imgHeight, 1:imgWidth);
+
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(D_denoise_atv);
+set(gca,'dataAspectRatio',[1 1 1])
+title('Depth Denoised (standard)')
+
+% Denoising using adaptive way
+IR_resize = [IR_undistort; zeros(imgWidth-imgHeight, imgWidth)];
+IR_unrolled = IR_resize(:);
+IR_unrolled = double(IR_unrolled);
+
+c_thres_rate = 0.005 % contolls the threshold c
+c = c_thres_rate * (max(IR_unrolled))^2;
+
+weight_fidelity = 1/c * min(c, IR_unrolled.^2); % check the formula in tof.pdf
+%weight_fidelity = ones(length(IR_unrolled),1);
+
+D_unrolled_denoise_atv_adaptive = SB_ATV_weighted(D_unrolled, mu_reg, weight_fidelity);
+fprintf('ATV Rel.Err = %g\n',norm(D_unrolled_denoise_atv_adaptive(:) - D_unrolled(:)) ...
+    / norm(D_unrolled_denoise_atv_adaptive(:)));
+
+D_denoise_atv_adaptive = reshape(D_unrolled_denoise_atv_adaptive, imgWidth, imgWidth);
+D_denoise_atv_adaptive = D_denoise_atv_adaptive(1:imgHeight, 1:imgWidth);
+
+figure(image_counter);
+image_counter = image_counter + 1;
+imagesc(D_denoise_atv_adaptive);
+set(gca,'dataAspectRatio',[1 1 1])
+title('Depth Denoised (adaptive£©')
 
 %% Construct Point Cloud
 pc = tof2pc(D_undistort, C);
@@ -37,19 +114,41 @@ zlabel('Z')
 
 originpc = pc;
 
-%% RANSAC
+%% Construct Point Cloud - for denoised (standard)
+pc_denoised = tof2pc(D_denoise_atv, C);
+figure(image_counter);
+image_counter = image_counter + 1;
+pcshow(pc_denoised)
+title('Pointcloud Denoised (standard)')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
+
+
+%% Construct Point Cloud - for denoised (adaptive)
+pc_denoised_adaptive = tof2pc(D_denoise_atv_adaptive, C);
+figure(image_counter);
+image_counter = image_counter + 1;
+pcshow(pc_denoised_adaptive)
+title('Pointcloud Denoised (adaptive)')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
+
+%% RANSAC: need to specify noise_thres
 pc = originpc;
 
 iterations = 100;
-subset_size = 3;
+subset_size = 3; %number of points to specify a plane
 
 figure(image_counter);
 hold on;
 image_counter = image_counter + 1;
-numplanes = 4;
+numplanes = 4; %number of planes to fit
 plane_models = zeros(numplanes,4);
-for i = 1:numplanes
-    noise_ths = ones(1, length(pc)) * 20;
+noise_thres = 20; %noise threshold
+for i = 1:numplanes %fit the plane, than take out the plane
+    noise_ths = ones(1, length(pc)) * noise_thres;
     [plane_models(i,:), outlier_ratio, plane_area, inliers, best_inliers] ...
         = ransac_fitplane(pc, 1:length(pc), noise_ths, iterations, subset_size);
     pc(best_inliers, :) = [];
@@ -62,7 +161,62 @@ xlabel('X');
 ylabel('Y');
 zlabel('Z');
 
-%% calculate intersection line
+
+%% RANSAC - denoised (standard)
+%pc = originpc;
+pc = pc_denoised;
+
+iterations = 100;
+subset_size = 3; %number of points to specify a plane
+
+figure(image_counter);
+hold on;
+image_counter = image_counter + 1;
+numplanes = 4; %number of planes to fit
+plane_models = zeros(numplanes,4);
+noise_thres = 20; %noise threshold
+for i = 1:numplanes %fit the plane, than take out the plane
+    noise_ths = ones(1, length(pc)) * noise_thres;
+    [plane_models(i,:), outlier_ratio, plane_area, inliers, best_inliers] ...
+        = ransac_fitplane(pc, 1:length(pc), noise_ths, iterations, subset_size);
+    pc(best_inliers, :) = [];
+    plane_points{i} = inliers;
+    pcshow(inliers, [bitshift(bitand(i,4),-2) bitshift(bitand(i,2),-1) bitand(i,1)]);
+end
+% plane1 blue, plane2 green, plane3 cyan, plane4 red
+title('inliers - denoised (standard)')
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+
+%% RANSAC - denoised (adaptive)
+%pc = originpc;
+pc = pc_denoised_adaptive;
+
+iterations = 100;
+subset_size = 3; %number of points to specify a plane
+
+figure(image_counter);
+hold on;
+image_counter = image_counter + 1;
+numplanes = 4; %number of planes to fit
+plane_models = zeros(numplanes,4);
+noise_thres = 20; %noise threshold
+for i = 1:numplanes %fit the plane, than take out the plane
+    noise_ths = ones(1, length(pc)) * noise_thres;
+    [plane_models(i,:), outlier_ratio, plane_area, inliers, best_inliers] ...
+        = ransac_fitplane(pc, 1:length(pc), noise_ths, iterations, subset_size);
+    pc(best_inliers, :) = [];
+    plane_points{i} = inliers;
+    pcshow(inliers, [bitshift(bitand(i,4),-2) bitshift(bitand(i,2),-1) bitand(i,1)]);
+end
+% plane1 blue, plane2 green, plane3 cyan, plane4 red
+title('inliers - denoised (adaptive)')
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+
+%% calculate intersection line (not needed)
 % plot the original planes
 % syms X Y Z1 Z2
 % p1=plane_models(1,:);
@@ -80,7 +234,7 @@ zlabel('Z');
 % hold on;
 % surf(X,Y,Z2);
 
-% find intersection line
+%% find intersection line
 edges=zeros(numplanes.*(numplanes-1)./2-1, 6);
 k=1;
 for i=1:numplanes-1
@@ -117,15 +271,13 @@ hold off;
 figure(image_counter);
 hold on;
 image_counter = image_counter + 1;
-pcshow(plane_points{1});
-pcshow(plane_points{4});
+pcshow(plane_points{1}); % always the ground
+pcshow(plane_points{2}); % need to specify by hand
 title('parallel planes for height calculation');
-height = plane_dist(plane_models(1,:), plane_models(4,:), plane_points{1}, plane_points{4});
+height = plane_dist(plane_models(1,:), plane_models(2,:), plane_points{1}, plane_points{2});
 % height = 340.7499 / 341.3144
 
 %% calculate width and length
 figure(image_counter);
 image_counter = image_counter + 1;
-pcshow(plane_points{2});
-
-
+pcshow(plane_points{3}); % also, need to specify by hand
