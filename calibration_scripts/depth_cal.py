ae30201dba8d1871c5803e4ae82895cc39809ba7
Kun Huang <huangkun@umich.edu>
2020-07-22 00:13:48 -0400

    depth calibration examined

--- calibration_scripts/depth_cal.py
+++ calibration_scripts/depth_cal.py
@@ -16,14 +16,17 @@ def draw(img, corner, imgpts):
     return img
 
 
-def calibrateDepth(square_size, width=6, height=5, irPrefix='GrayImage_', depthPrefix='DepthImage_',
-                   imgID='*', image_format='png', dirpath='../data/calibration0712/plane*', ifVisualization=False):
+def calibrateDepth(square_size, mtx, dist, width=6, height=5,
+                   irPrefix='GrayImage_', depthPrefix='DepthImage_', rgbPrefix='RGBImage_',
+                   imgID='*', image_format='png', dirpath='../data/calibration0712/plane*',
+                   ifVisualization=False, ifDivide16=False, ifUseRGB=True):
     gtDistances = []
     tofs = []
     """ Apply camera calibration operation for images in the given directory path. """
     objp = np.zeros((height*width, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2) * square_size
-    axis_length = 50
+    objp[:, :2] = np.mgrid[0:width,
+                           0:height].T.reshape(-1, 2) * square_size  # n x 3 matrix
+    axis_length = 2 * square_size
     axis = np.float32([[axis_length, 0, 0], [0, axis_length, 0], [
                       0, 0, -axis_length]]).reshape(-1, 3)
 
@@ -40,23 +43,86 @@ def calibrateDepth(square_size, width=6, height=5, irPrefix='GrayImage_', depthP
         path, _ = os.path.split(fname)
         tofimg = cv2.imread(path + '/' + depthPrefix +
                             fileID + '.' + image_format, -1)
-        print(tofimg.dtype)
-        if ifVisualization:
-            cv2.imshow('tof image', tofimg)
+        print("tof image type: ", tofimg.dtype)
+
+        if ifUseRGB:
+            rgbimg = cv2.imread(path + '/' + rgbPrefix +
+                                fileID + '.' + image_format, -1)
+            print("rgb image type: ", rgbimg.dtype)
+            rgbgray = cv2.cvtColor(rgbimg, cv2.COLOR_BGR2GRAY)
+            retRGB, cornersRGB = cv2.findChessboardCorners(
+                rgbgray, (width, height), None)
 
         irimg = cv2.imread(fname)
         gray = cv2.cvtColor(irimg, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
-        if ret == True:
+        ret, corners = cv2.findChessboardCorners(
+            gray, (width, height), None)  # corners: 1 x 2 matrix
+        if ret == False:
+            print("Corners not found")
+        else:
             corners2 = cv2.cornerSubPix(
                 gray, corners, (11, 11), (-1, -1), criteria)
-            # Find the rotation and translation vectors.
+            if ifVisualization:
+                cornerimg = irimg.copy()
+                cornerimg = cv2.drawChessboardCorners(
+                    cornerimg, (width, height), corners2, ret)
+                cv2.imshow('chessboard', cornerimg)
+                cv2.waitKey(0)
+
+            """ Find the rotation and translation vectors. """
             ret, rvecs, tvecs = cv2.solvePnP(objp, corners2, mtx, dist)
+            # _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
             print("translation: ", tvecs)
             rvecsM, _ = cv2.Rodrigues(rvecs)
             print("rotation matrix: ", rvecsM)
-            """ project 3D corner points to image plane """
+
+            if ifUseRGB:
+                cornersRGB2 = cv2.cornerSubPix(
+                    rgbgray, cornersRGB, (11, 11), (-1, -1), criteria)
+                # Find the rotation and translation vectors.
+                retRGB, rvecsRGB, tvecsRGB = cv2.solvePnP(
+                    objp, cornersRGB2, rgbIntrinsic, rgbDist)
+                print("translation in RGB: ", tvecsRGB)
+
+
+
+            """ project 3D corners to image plane """
             for i in range(len(corners2)):
+                """ project 4 corners to image plane """
+                if i == 0 or i == width - 1 or i == width * (height - 1) or i == width * height - 1:
+                    if i == 0:
+                        print("-------------- top left corner --------------")
+                        objptl = objp[i].reshape((3, 1)) + np.float32([-square_size, -square_size, 0]).reshape(3, 1)
+                    elif i == width - 1:
+                        print("-------------- top right corner --------------")
+                        objptl = objp[i].reshape((3, 1)) + np.float32([square_size, -square_size, 0]).reshape(3, 1)
+                    elif i == width * (height - 1):
+                        print("-------------- bottom left corner --------------")
+                        objptl = objp[i].reshape((3, 1)) + np.float32([-square_size, square_size, 0]).reshape(3, 1)
+                    elif i == width * height - 1:
+                        print("-------------- bottom right corner --------------")
+                        objptl = objp[i].reshape((3, 1)) + np.float32([+square_size, square_size, 0]).reshape(3, 1)
+
+                    pt3 = rvecsM.dot(objptl) + tvecs
+                    print("This corner is from ", objptl.reshape((1, 3)), " to ", pt3.reshape((1, 3)))
+                    gtDist = np.linalg.norm(pt3)
+                    print("ground truth distance: ", gtDist)
+                    gtDistances.append(gtDist)
+
+                    pt2d, jac = cv2.projectPoints(
+                        pt3, np.identity(3), np.zeros((3, 1)), mtx, dist)
+                    print("projected corner coordinate: ", pt2d)  # 1 x 1 x 2 matrix
+
+                    # corners2: x, y!!! not row, col!!!
+                    tof = tofimg[int(pt2d[0][0][1]), int(pt2d[0][0][0])]
+                    print("tof image pixel value original: ", bin(tof))
+
+                    if ifDivide16:
+                        tof /= 16.0
+                    print("tof image pixel value: ", tof)
+                    tofs.append(tof)
+
+
                 print("-------------- corner ", i, " --------------")
                 pt3 = rvecsM.dot(objp[i].reshape((3, 1))) + tvecs
                 print("This corner is from ",
@@ -65,16 +131,28 @@ def calibrateDepth(square_size, width=6, height=5, irPrefix='GrayImage_', depthP
                 print("ground truth distance: ", gtDist)
                 gtDistances.append(gtDist)
 
-                print("2D coordinate: ", corners2[i])
+                print("2D Corner coordinate: ", corners2[i])
+                pt2d, jac = cv2.projectPoints(
+                    pt3, np.identity(3), np.zeros((3, 1)), mtx, dist)
+                print("projected corner coordinate: ", pt2d)
+
                 # corners2: x, y!!! not row, col!!!
                 tof = tofimg[int(corners2[i][0][1]), int(corners2[i][0][0])]
+                print("tof image pixel value original: ", bin(tof))
+
+                # # switch 2 channels: tof = tofc1 | tofc2
+                # tofc1 = tof >> 8
+                # tofc2 = (tof << 8) % (1 << 16)
+                # # tof = (((tof << 8) + (tof >> 8)) % (1 << 16)) >> 4
+                if ifDivide16:
+                    tof /= 16.0
                 print("tof image pixel value: ", tof)
                 tofs.append(tof)
 
                 if ifVisualization:
                     imgMark = irimg.copy()
                     imgpts, jac = cv2.projectPoints(
-                        objp[i]+axis, rvecs, tvecs, mtx, dist)
+                        objp[i]+axis, rvecs, tvecs, mtx, dist) # objp[i]: 3 vector
                     imgMark = draw(imgMark, corners2[i], imgpts)
                     cv2.imshow('imgMark', imgMark)
                     cv2.waitKey(0)
@@ -84,7 +162,11 @@ def calibrateDepth(square_size, width=6, height=5, irPrefix='GrayImage_', depthP
 
 if __name__ == '__main__':
     # Load previously saved data
-    mtx, dist = load_coefficients('irCamera.yml')
+    irIntrinsic, irDist = load_coefficients('irCamera.yml')
+    print("TOF Camera Intrinsic Matrix: ", irIntrinsic)
+
+    rgbIntrinsic, rgbDist = load_coefficients('rgbCamera.yml')
+    print("RGB Camera Intrinsic Matrix: ", rgbIntrinsic)
 
     square_size = 26.3571  # 13.1923  #26.3571
     width = 6  # 13 #6
@@ -93,23 +175,38 @@ if __name__ == '__main__':
     allgtDistances = []
     alltofs = []
 
-    dirpath = '../data/calibration0712/plane*'
-    gtDistances, tofs = calibrateDepth(square_size=square_size, width=width,
-                                       height=height, imgID='*', dirpath=dirpath)
+    dirpath = '../data/calibration0720/corner*'
+    gtDistances, tofs = calibrateDepth(square_size=square_size, mtx=irIntrinsic, dist=irDist,
+                                       irPrefix='Gray_', depthPrefix='DepthImage_',
+                                       width=width, height=height, imgID='*', dirpath=dirpath,
+                                       ifVisualization=False, ifDivide16=True, ifUseRGB=False)
     allgtDistances += gtDistances
     alltofs += tofs
 
-    dirpath = '../data/calibration0716/1'
-    gtDistances, tofs = calibrateDepth(square_size=square_size, width=width,
-                                       height=height, imgID='*', dirpath=dirpath)
-    allgtDistances += gtDistances
-    alltofs += tofs
+    # dirpath = '../data/parallelPlaneTocheckerboard/Plane50'
+    # gtDistances, tofs = calibrateDepth(square_size=30, mtx=irIntrinsic, dist=irDist,
+    #                                    width=7, height=5, imgID='*', dirpath=dirpath,
+    #                                    ifVisualization=True, ifDivide16=True, ifUseRGB=False)
+    # allgtDistances += gtDistances
+    # alltofs += tofs
 
-    dirpath = '../data/calibration0716/2'
-    gtDistances, tofs = calibrateDepth(square_size=square_size, width=width,
-                                       height=height, imgID='*', dirpath=dirpath)
-    allgtDistances += gtDistances
-    alltofs += tofs
+    # dirpath = '../data/calibration0712/plane*'
+    # gtDistances, tofs = calibrateDepth(square_size=square_size, mtx=irIntrinsic, dist=irDist,
+    #                                    width=width, height=height, imgID='*', dirpath=dirpath, ifVisualization=False)
+    # allgtDistances += gtDistances
+    # alltofs += tofs
+
+    # dirpath = '../data/calibration0716/1'
+    # gtDistances, tofs = calibrateDepth(square_size=square_size, mtx=irIntrinsic, dist=irDist,
+    #                                    width=width, height=height, imgID='*', dirpath=dirpath, ifVisualization=True)
+    # allgtDistances += gtDistances
+    # alltofs += tofs
+
+    # dirpath = '../data/calibration0716/2'
+    # gtDistances, tofs = calibrateDepth(square_size=square_size, mtx=irIntrinsic, dist=irDist,
+    #                                    width=width, height=height, imgID='*', dirpath=dirpath, ifVisualization=True)
+    # allgtDistances += gtDistances
+    # alltofs += tofs
 
     plt.plot(alltofs, allgtDistances, 'ro')
 
